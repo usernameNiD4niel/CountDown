305e6896316e72b5237a277506457f94cfb1e00a
Daniel Rey <danielrey10252000@gmail.com>
2022-12-19 10:50:08 +0800

    The error fix, showing diff input

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
 const second = document.querySelector(".second");
 
-// I can't get the correct way to do this :(
 setInterval(()=> {
     const currentDate = new Date();
     const newYearsDate = new Date("1 Jan 2023");
-    const remaining = newYearsDate - currentDate;
-    
-    day.innerText = remaining.toString().substring(0,2);
-    hour.innerText = remaining.toString().substring(2,4);
-    minute.innerText = remaining.toString().substring(4,6);
-    second.innerText = remaining.toString().substring(6,8);
+    const remaining = (newYearsDate - currentDate) / 1000;
 
-}, 10);
+    const days = Math.floor(remaining / 3600 / 24);
+    const hours = Math.floor(remaining / 3600) % 24;
+    const minutes = Math.floor(remaining / 60) % 60;
+    const seconds = Math.floor(remaining) % 60;
+    
+    day.innerText = (days < 10) ? `0${days}` : days;
+    hour.innerText = hours;
+    minute.innerText = minutes;
+    second.innerText = (seconds < 10) ? `0${seconds}` : seconds;
+
+}, 1000);
